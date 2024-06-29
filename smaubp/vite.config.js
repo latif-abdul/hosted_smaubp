5b0e9b7203f3a6a540e01fdbd0cb04e35e959ce9
latif-abdul <54927430+latif-abdul@users.noreply.github.com>
2024-06-30 04:18:45 +0700

    add vite ssl

--- smaubp/vite.config.js
+++ smaubp/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     plugins: [
@@ -19,6 +20,7 @@ export default defineConfig({
                 },
             },
         }),
+        basicSsl(),
     ],
     resolve: {
         alias: {
