4fae0cb9153f4bd0582a0b2a01fb82c2b1601640
latif-abdul <54927430+latif-abdul@users.noreply.github.com>
2024-06-30 05:54:36 +0700

    inactive second jquery

--- smaubp/resources/views/footer.blade.php
+++ smaubp/resources/views/footer.blade.php
@@ -62,12 +62,12 @@
 <script>scrollCue.init();</script>
 
 <!-- Custom Js -->
-<script src="js/custom.js"></script>
+<!-- <script src="js/custom.js"></script> -->
 
 <!-- Add JS counter lib -->
 <script src="js/jquery.waypoints.min.js"></script>
 <script src="js/jquery.counterup.min.js"></script>
-<script src="http://code.jquery.com/jquery.js"></script>
+<!-- <script src="http://code.jquery.com/jquery.js"></script> -->
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 
