6cd9ea49b95e2c01c88bd91cd4ffafa649f467bc
latif-abdul <54927430+latif-abdul@users.noreply.github.com>
2024-06-30 07:14:32 +0700

    Update ppdb.blade.php

--- smaubp/resources/views/ppdb.blade.php
+++ smaubp/resources/views/ppdb.blade.php
@@ -150,7 +150,8 @@
                 <div class="shareArtikel mb-4">
 
                     <div class="containerIconShare">
-                        <a href="/form" class="btn btn-primary mr-3">
+                        <!-- <a href="/form" class="btn btn-primary mr-3"> -->
+                        <a href="https://bit.ly/44fRtwQ" class="btn btn-primary mr-3">
                             Daftar
                         </a>
 
