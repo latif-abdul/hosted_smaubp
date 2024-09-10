bed1ccd08f9eee3f64722b27d08cefa91d0a96c5
latif-abdul <54927430+latif-abdul@users.noreply.github.com>
2024-09-10 20:23:30 +0700

    Update ppdb.blade.php

--- smaubp/resources/views/ppdb.blade.php
+++ smaubp/resources/views/ppdb.blade.php
@@ -16,9 +16,9 @@
             </h4>
             <ol>
                 <li>Formulir pendaftaran Rp.400.000,-</li>
-                <li>Sarana prasarana Rp.7.000.000,-</li>
-                <li>Seragam Rp. 1.200.000,-</li>
-                <li>SPP per bulan Rp.1.950.000,-</li>
+                <li>Sarana prasarana Rp.7.500.000,-</li>
+                <li>Seragam Rp. 1.350.000,-</li>
+                <li>SPP per bulan Rp.2.050.000,-</li>
             </ol>
             <hr>
             <h4>JADWAL PENERIMAAN SANTRI BARU</h4>
