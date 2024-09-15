84ec7a7d2c7c068f17b3a0c3813bfa8d77c92252
latif-abdul <54927430+latif-abdul@users.noreply.github.com>
2024-09-15 10:23:40 +0700

    Update index.blade.php

--- smaubp/resources/views/index.blade.php
+++ smaubp/resources/views/index.blade.php
@@ -76,7 +76,21 @@
 
 <br>
 <h2 align="center">Program Tahfidzul Al-Qur'an</h2>
-<div class="container mainDetailArtikel">
+<div class="kepsek" data-cues="slideInUp">
+        <div class="container">
+        <img src="/Tahfidzul Qur_an/XII-7 bersama Gus-Ning.jpg" alt="Img Artikel" class="imgArtikel">
+        <div class="visiMisi">
+
+<!-- <div class="visi">
+                <h5>Our Vision</h5>
+                <h3>Terwujudnya manusia yang unggul, utuh dan berakhlakul karimah guna kemulyaan dan kejayaan Islam dan kaum muslimin , 
+                    kemulyaan dan kejayaan seluruh bangsa Indonesia dan untuk terwujudnya cita cita luhur kemerdekaan
+                    yaitu terwujudnya kesejahteraan dan tegaknya keadilan utamanya di negara Republik Indonesia.
+                </h3>
+            </div> -->
+
+<div class="misi">
+    <h5></h5>
     <p>Pelaksanaan program tahfidzul qur’an di SMAU BP dilaksanakan secara komprehensif setiap hari Senin – Jum’at
         dengan
         menerapkan 3 metode yaitu; tartil, tahfidz dan taqrir. Dari metode tersebut, santri SMAU BP mampu menerapkan
@@ -86,26 +100,11 @@
         yang mampu menghafal 30 juz. Pada tahun 2024 SMAU BP Amanatul Ummah mewisuda sejumlah 79 santri kategori 30 Juz,
         92 santri kategori
         15 Juz, 62 santri kategori 9 Juz</p>
-
-    <section id="portfolio" class="portfolio">
-        <div class="container">
-            <div class="row portfolio-container">
-                @foreach ($dir2 as $fileinfo)
-                    @if (!$fileinfo->isDot())
-                        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                            <div class="portfolio-wrap">
-                                <img src="/Tahfidzul Qur_an/{{$fileinfo->getFilename()}}" class="img-fluid" alt="">
-                                <div class="portfolio-info">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-        </div>
-    </section>
 </div>
+
+</div>
+        </div>
+        </div>
 
 <!-- jurusan -->
 <div class="jurusan">
